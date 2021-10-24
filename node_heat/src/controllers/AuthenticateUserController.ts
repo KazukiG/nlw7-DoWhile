96d4808e61680c28428a821676846cae48e749fb
KazukiG <kazukigushiken20@gmail.com>
2021-10-24 20:56:26 -0300

    dia 3

--- node_heat/src/controllers/AuthenticateUserController.ts
+++ node_heat/src/controllers/AuthenticateUserController.ts
@@ -1,19 +1,29 @@
-
 import { Request, Response } from "express";
 import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response) {
-    const { code } = request.body;
+    const { code, serviceType } = request.body;
+    const services = new AuthenticateUserService();
 
-    const service = new AuthenticateUserService();
+
     try {
-      const result = await service.execute(code);
-      return response.json(result);
+      if (serviceType == 'web' || serviceType == undefined) {
+        const result = await services.execute(code, 'web');
+        return response.json(result);
+      }
+      if (serviceType == 'mobile') {
+        const result = await services.execute(code, 'mobile');
+        return response.json(result);
+      }
+
     } catch (err) {
+      console.log("Server: User authentication: FAILED")
       return response.json({ error: err.message });
     }
+
   }
 }
+
 
 export { AuthenticateUserController };
--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -1,22 +1,13 @@
 import axios from "axios";
-import prismaClient from "../prisma";
-import { sign } from "jsonwebtoken";
+import prismaClient from "../prisma"
+import { sign } from 'jsonwebtoken'
 
-/**
- * Receber code(string)
- * Recuperar o access_token no github
- * Recuperar infos do user no github
- * Verificar se o usuario existe no DB
- * ---- SIM = Gera um token
- * ---- NAO = Cria no DB, gera um token
- * Retornar o token com as infos do user
- */
 
-interface IAccessTokenResponse {
+type IAccessTokenResponse = {
   access_token: string;
 }
 
-interface IUserResponse {
+type IUserResponse = {
   avatar_url: string;
   login: string;
   id: number;
@@ -24,37 +15,57 @@ interface IUserResponse {
 }
 
 class AuthenticateUserService {
-  async execute(code: string) {
+
+  async execute(code: string, serviceType: 'web' | 'mobile' = 'web') {
+
+    console.log("Server: User authentication, serviceType: " + serviceType)
+
     const url = "https://github.com/login/oauth/access_token";
 
-    const { data: accessTokenResponse } =
-      await axios.post<IAccessTokenResponse>(url, null, {
-        params: {
-          client_id: process.env.GITHUB_CLIENT_ID,
-          client_secret: process.env.GITHUB_CLIENT_SECRET,
-          code,
-        },
-        headers: {
-          Accept: "application/json",
-        },
-      });
+    // sistema para o backend rodar em web e mobile, o mesmo backend.
+    // caso necessite de mais sistemas, só ir adicionando as configurações e no .env
 
-    const response = await axios.get<IUserResponse>(
-      "https://api.github.com/user",
-      {
-        headers: {
-          authorization: `Bearer ${accessTokenResponse.access_token}`,
-        },
+    const envServiceTypes_CLIENT_ID = {
+      web: process.env.GITHUB_WEB_CLIENT_ID,
+      mobile: process.env.GITHUB_MOBILE_CLIENT_ID
+    }
+    const envServiceTypes_CLIENT_SECRET = {
+      web: process.env.GITHUB_WEB_CLIENT_SECRET,
+      mobile: process.env.GITHUB_MOBILE_CLIENT_SECRET
+    }
+
+    const CLIENT_ID = envServiceTypes_CLIENT_ID[serviceType]
+    const CLIENT_SECRET = envServiceTypes_CLIENT_SECRET[serviceType]
+
+
+
+
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        "Accept": "application/json"
       }
-    );
+    })
+    // console.log(accessTokenResponse)
+
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    })
 
     const { login, id, avatar_url, name } = response.data;
 
     let user = await prismaClient.user.findFirst({
       where: {
-        github_id: id,
-      },
-    });
+        github_id: id
+      }
+    })
 
     if (!user) {
       user = await prismaClient.user.create({
@@ -62,28 +73,33 @@ class AuthenticateUserService {
           github_id: id,
           login,
           avatar_url,
-          name,
-        },
-      });
+          name
+        }
+      })
     }
 
     const token = sign(
       {
         user: {
           name: user.name,
-          avatar_ur: user.avatar_url,
+          avatar_url: user.avatar_url,
           id: user.id,
-        },
+        }
       },
       process.env.JWT_SECRET,
       {
         subject: user.id,
-        expiresIn: "1d",
+        expiresIn: "1d"
       }
-    );
+    )
+    console.log("Server: User authentication: SUCESS")
+
+    //se não existir access token retorna um aviso!
 
     return { token, user };
+
   }
+
 }
 
-export { AuthenticateUserService };
+export { AuthenticateUserService }
